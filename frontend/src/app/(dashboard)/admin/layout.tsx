97ff7027a76568a68017261241d0d659b2107f43
PKirill-106 <p.kirill.design@gmail.com>
2025-08-18 19:32:28 +0200

    feat: improve UI on admin's layout by showing active page on sidebar

--- frontend/src/app/(dashboard)/admin/layout.tsx
+++ frontend/src/app/(dashboard)/admin/layout.tsx
@@ -1,13 +1,9 @@
-import LogoutButton from '@/components/UI/LogoutButton'
+import MySidebar from '@/components/admin/MySidebar'
 import {
-	Sidebar,
-	SidebarGroup,
-	SidebarMenuButton,
-	SidebarProvider,
+	SidebarProvider
 } from '@/components/UI/sidebar'
 import { authOptions } from '@/lib/auth'
 import { getServerSession } from 'next-auth'
-import Link from 'next/link'
 export default async function AdminLayout({
 	children,
 }: {
@@ -21,23 +17,7 @@ export default async function AdminLayout({
 	return (
 		<SidebarProvider>
 			<div className='container flex gap-10 md:grid md:grid-cols-4'>
-				<Sidebar className='md:col-span-1'>
-					<SidebarGroup title='Admin Menu'>
-						<SidebarMenuButton asChild className='border-1'>
-							<Link href='/admin/products'>Продукти</Link>
-						</SidebarMenuButton>
-						<SidebarMenuButton asChild className='border-1'>
-							<Link href='/admin/banners'>Банери</Link>
-						</SidebarMenuButton>
-						<SidebarMenuButton asChild className='p-2 border-1'>
-							<Link href='/admin/seasons'>Сезони</Link>
-						</SidebarMenuButton>
-						<SidebarMenuButton asChild className='p-2 border-1'>
-							<Link href='/admin/discounts'>Знижки</Link>
-						</SidebarMenuButton>
-						<LogoutButton />
-					</SidebarGroup>
-				</Sidebar>
+				<MySidebar />
 				<main className='md:col-span-3'>{children}</main>
 			</div>
 		</SidebarProvider>
---
+++ frontend/src/components/admin/MySidebar.tsx
@@ -0,0 +1,36 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import LogoutButton from '../UI/LogoutButton'
+import { Sidebar, SidebarGroup, SidebarMenuButton } from '../UI/sidebar'
+
+export default function MySidebar() {
+	const pathname = usePathname()
+
+	const currentClass = (path: string) => {
+		const isActive = pathname.startsWith(path)
+		return isActive
+			? 'bg-button text-button-text font-semibold pointer-events-none'
+			: 'border-1'
+	}
+
+	return (
+		<Sidebar className='md:col-span-1'>
+			<SidebarGroup title='Admin Menu'>
+				<SidebarMenuButton asChild className={currentClass('/admin/products')}>
+					<Link href='/admin/products'>Продукти</Link>
+				</SidebarMenuButton>
+				<SidebarMenuButton asChild className={currentClass('/admin/banners')}>
+					<Link href='/admin/banners'>Банери</Link>
+				</SidebarMenuButton>
+				<SidebarMenuButton asChild className={currentClass('/admin/seasons')}>
+					<Link href='/admin/seasons'>Сезони</Link>
+				</SidebarMenuButton>
+				<SidebarMenuButton asChild className={currentClass('/admin/discounts')}>
+					<Link href='/admin/discounts'>Знижки</Link>
+				</SidebarMenuButton>
+				<LogoutButton />
+			</SidebarGroup>
+		</Sidebar>
+	)
+}
